7178bb821c10c1add0800bc71939df7d2aedd0c9
holynova <holy_nova@126.com>
2021-12-10 17:54:44 +0800

    生成拼音字库

--- backend/makeDict.js
+++ backend/makeDict.js
@@ -1,11 +1,25 @@
 const rawData = require("./raw/raw.json");
+
 const log = console.log.bind(console);
 const { saveToFile } = require("./saveToFile");
+const { pinyin } = require("pinyin-pro");
 
 function saveByChar(dict, outputDir) {
   Object.entries(dict).forEach(([k, v]) => {
     saveToFile({ data: JSON.stringify(v), fileName: `${k}.json`, outputDir });
   });
+}
+function convertCharDictToPinyinDict(dict) {
+  const res = {};
+  Object.entries(dict).forEach(([char, poemList]) => {
+    const pinyinKey = pinyin(char, { toneType: "num" });
+    if (pinyinKey in res) {
+      res[pinyinKey] = [...res[pinyinKey], ...poemList];
+    } else {
+      res[pinyinKey] = poemList;
+    }
+  });
+  return res;
 }
 
 function main() {
@@ -20,7 +34,10 @@ function main() {
   });
 
   saveByChar(headWordDict, "./output/head");
+  saveByChar(convertCharDictToPinyinDict(headWordDict), "./output/head_pinyin");
   saveByChar(tailWordDict, "./output/tail");
+  saveByChar(convertCharDictToPinyinDict(tailWordDict), "./output/tail_pinyin");
+
   console.timeEnd();
   log("done");
 }
