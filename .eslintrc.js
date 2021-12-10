7ced81ed159d74879c644fa5e37a764da3fcc14e
holynova <holy_nova@126.com>
2021-12-10 16:11:15 +0800

    修复eslint错误

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,21 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ["plugin:react/recommended", "airbnb"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    "extends": [
-        "plugin:react/recommended",
-        "airbnb"
-    ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaFeatures": {
-            "jsx": true
-        },
-        "ecmaVersion": 13,
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react",
-        "@typescript-eslint"
-    ],
-    "rules": {
-    }
+    ecmaVersion: 13,
+    sourceType: "module",
+  },
+  plugins: ["react", "@typescript-eslint", "react-hooks"],
+  rules: {
+    quotes: 0,
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "error",
+  },
 };
--- src/pages/PoemPage.tsx
+++ src/pages/PoemPage.tsx
@@ -5,22 +5,12 @@ import Button from "antd-mobile/es/components/button";
 import Selector from "antd-mobile/es/components/selector";
 import List from "antd-mobile/es/components/list";
 
-import poemDict from "../common/dict/poem.json";
-
-const log = console.log.bind(console);
-import * as rand from "../utils/rand";
+import { Space } from "_antd-mobile@5.0.0-rc.3@antd-mobile";
 import "./PoemPage.scss";
-import {
-  HeadTailDict,
-  HeadTailItem,
-  PoemDictItemModel,
-  PoemDictModel,
-  ResultRowModel,
-  RowDataModel,
-} from "./poem";
+import { HeadTailDict, HeadTailItem, RowDataModel } from "./poem";
+
 import PoemRow from "./components/PoemRow";
 import Author from "../common/components/Author";
-import { Space } from "_antd-mobile@5.0.0-rc.3@antd-mobile";
 
 function getModuleFullName(isHead = true, name = "*") {
   return `../common/dict/${isHead ? "head" : "tail"}/${name}.json`;
@@ -30,18 +20,18 @@ const headModules = import.meta.glob("../common/dict/head/*.json");
 const tailModules = import.meta.glob("../common/dict/tail/*.json");
 
 const loadChosenDicts = (isHead = true, names: string[]) => {
-  let pList = names.map((x) => {
-    let fullName = getModuleFullName(isHead, x);
-    let moduleDict = isHead ? headModules : tailModules;
+  const pList = names.map((x) => {
+    const fullName = getModuleFullName(isHead, x);
+    const moduleDict = isHead ? headModules : tailModules;
     if (typeof moduleDict[fullName] === "function") {
       return moduleDict[fullName]();
     }
     return Promise.reject(new Error(`${fullName}不存在`));
   });
   return Promise.allSettled(pList).then((arr) => {
-    let dict: HeadTailDict = {};
+    const dict: HeadTailDict = {};
     arr.forEach((promiseResult, index) => {
-      let key = names[index];
+      const key = names[index];
       if (promiseResult.status === "fulfilled")
         dict[key] = promiseResult.value.default;
     });
@@ -53,30 +43,17 @@ const loadChosenDicts = (isHead = true, names: string[]) => {
 const buttonProps = {
   // size: "small",
 };
-function PoemPage() {
-  let [size, setSize] = useState([7]);
-  let [position, setPosition] = useState(["HEAD"]);
-  let [keyWord, setKeyWord] = useState("清风明月水落石出");
-  let [rows, setRows] = useState<RowDataModel[]>([]);
-  let [results, setResults] = useState<string[]>([]);
-  let [sourceVisible, setSourceVisible] = useState(true);
-  const genRandomRowData = (resultList: HeadTailItem[]): ResultRowModel => {
-    let count = resultList.length;
-    let randomIndex = rand.between(0, count);
-    let row = resultList[randomIndex];
-    // let row = rand.choose(found)
-    return {
-      ...row,
-      alter: resultList,
-      count,
-      index: randomIndex,
-    };
-  };
+const PoemPage = function () {
+  const [size, setSize] = useState([7]);
+  const [position, setPosition] = useState(["HEAD"]);
+  const [keyWord, setKeyWord] = useState("清风明月水落石出");
+  const [rows, setRows] = useState<RowDataModel[]>([]);
+  const [sourceVisible, setSourceVisible] = useState(true);
 
   const composePoem = useCallback(
     (word = "", length = 7) => {
-      let rowDataList: RowDataModel[] = [];
-      let input: string = word.trim();
+      const rowDataList: RowDataModel[] = [];
+      const input: string = word.trim();
       if (!input) {
         return;
       }
@@ -90,7 +67,7 @@ function PoemPage() {
               results: [],
             };
 
-            let found: HeadTailItem[] = mergedHeadTailDict[char];
+            const found: HeadTailItem[] = mergedHeadTailDict[char];
             // const notFoundData: ResultRowModel = {
             //   line: "翻遍字典没找到，你快过来自己编",
             //   poemId: "",
@@ -105,10 +82,12 @@ function PoemPage() {
               // 将找到的诗句, 根据字数来放到字典里, 以便区分五言, 七言等
               // key 为字数, value 为诗句
               const groupByLengthDict: LengthDictModel = found.reduce(
-                (pre, cur, index) => {
-                  let res: LengthDictModel = { ...pre };
+                (pre, cur) => {
+                  const res: LengthDictModel = { ...pre };
                   // 减去中间的标点1个字符, 再除以二, 就是半句的长度
-                  let key: string = `${Math.floor((cur.line.length - 1) / 2)}`;
+                  const key: string = `${Math.floor(
+                    (cur.line.length - 1) / 2
+                  )}`;
                   if (key in res) {
                     res[key] = [...res[key], cur];
                   } else {
@@ -119,7 +98,7 @@ function PoemPage() {
                 {}
               );
 
-              let results = groupByLengthDict[`${length}`];
+              const results = groupByLengthDict[`${length}`];
               // let rowData = results ? genRandomRowData(results) : notFoundData;
               // log("debug", { lengthDict, rowData, found });
               // let rowData = results || [];
@@ -146,7 +125,7 @@ function PoemPage() {
             <div className="title">藏头诗生成器</div>
           </List.Item>
           <List.Item title="请输入">
-            <Input value={keyWord} onChange={setKeyWord} clearable></Input>
+            <Input value={keyWord} onChange={setKeyWord} clearable />
           </List.Item>
           <List.Item>
             <Selector
@@ -157,7 +136,7 @@ function PoemPage() {
                 { label: "七律", value: 7 },
                 { label: "五绝", value: 5 },
               ]}
-            ></Selector>
+            />
           </List.Item>
           <List.Item>
             <Selector
@@ -168,7 +147,7 @@ function PoemPage() {
                 { label: "藏尾", value: "TAIL" },
               ]}
               onChange={setPosition}
-            ></Selector>
+            />
           </List.Item>
         </List>
 
@@ -198,21 +177,17 @@ function PoemPage() {
         {/* <div className="author">李白</div> */}
         <List>
           {rows.map((x, index) => (
-            <PoemRow
-              key={index}
-              data={x}
-              sourceVisible={sourceVisible}
-            ></PoemRow>
+            <PoemRow key={index} data={x} sourceVisible={sourceVisible} />
           ))}
         </List>
       </div>
 
-      <Author></Author>
+      <Author />
       {/* <pre>
         {JSON.stringify({ size, keyWord, position }, null, 2)}
       </pre> */}
     </div>
   );
-}
+};
 
 export default PoemPage;
